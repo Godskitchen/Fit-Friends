4667a4e54e13fcc13a1591683865621e50d5a484
Godskitchen <diemygod@gmail.com>
2023-11-12 03:42:08 +0300

    feat: CRA

---
+++ frontend/fit-friends/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  ignorePatterns: ['.eslintrc.js'],
+  plugins: ['@typescript-eslint'],
+  overrides: [
+    {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      project: 'tsconfig.json',
+      tsconfigRootDir: __dirname,
+      sourceType: 'module',
+    },
+    files: ["./src/**/*.ts", "./src/**/*.tsx", "*test*"],
+    extends: [
+      "react-app",
+      "react-app/jest",
+      "htmlacademy/react-typescript"
+    ],
+    rules: {
+      "@typescript-eslint/unbound-method": "off",
+      "jest/unbound-method": "error"
+    },
+    }
+  ]
+};
